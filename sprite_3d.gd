a1cc225628e1f8e22b22dc966daac49edbbfd293
AlexDavis <boobooca10@gmail.com>
2025-02-03 18:06:41 -0800

    Revert to 4.3

--- sprite_3d.gd
+++ sprite_3d.gd
@@ -1,6 +1,5 @@
 extends Node3D
 
-
-
 func _process(delta: float) -> void:
-	global_position.x += 1 * delta
+	global_position.x += Input.get_last_mouse_velocity().x * delta * 0.01
+	global_position.z += Input.get_last_mouse_velocity().y * delta * 0.01
