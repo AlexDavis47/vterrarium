ed236f5a207b28a5add1703b2da5d6227ebc8341
AlexDavis <boobooca10@gmail.com>
2025-02-10 16:07:09 -0800

    random fish colors

--- float_npc.gd
+++ float_npc.gd
@@ -3,14 +3,14 @@ extends CharacterBody3D
 @export var bounds_min: Vector3 = Vector3(-5, -5, -5)
 @export var bounds_max: Vector3 = Vector3(5, 5, 5)
 @export var speed: float = 2.0
-#@export var mesh1: MeshInstance3D
-#@export var mesh2: MeshInstance3D
+@export var sprite1: Sprite3D
+@export var sprite2: Sprite3D
 
 var target_position: Vector3
 
 func _ready():
 	randomize()
-	#randomize_color()
+	randomize_color()
 	pick_new_target()
 
 func _process(delta):
@@ -26,26 +26,13 @@ func pick_new_target():
 func move_towards_target(delta):
 	var direction = (target_position - global_transform.origin).normalized()
 	velocity = direction * speed
-
 	if global_transform.origin.distance_to(target_position) < 0.5:
 		pick_new_target()
-
 	move_and_slide()
-	
+
 func randomize_color():
-	# Create new materials to ensure uniqueness
-	var mat1 = StandardMaterial3D.new()
-	var mat2 = StandardMaterial3D.new()
-	var color: Color = Color(randf(), randf(), randf(), 1)
-
-	# Assign unique random colors
-	mat1.albedo_color = color
-	mat2.albedo_color = color
-
-	## Ensure each instance gets a unique mesh copy
-	#mesh1.mesh = mesh1.mesh.duplicate(true)
-	#mesh2.mesh = mesh2.mesh.duplicate(true)
-#
-	## Assign the new materials to the newly duplicated meshes
-	#mesh1.set_surface_override_material(0, mat1)
-	#mesh2.set_surface_override_material(0, mat2)
+	var new_color = Color(randf(), randf(), randf(), 1.0)
+	if sprite1:
+		sprite1.modulate = new_color
+	if sprite2:
+		sprite2.modulate = new_color
