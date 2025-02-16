040c64a184db89df85f26fde4b2a5aa4776bf818
Alex Davis <a_davis47@u.pacific.edu>
2025-02-16 12:36:37 -0800

    Add new global singletons and rename existing ones

--- addons/vterrariumframework/vterrariumframework.gd
+++ addons/vterrariumframework/vterrariumframework.gd
@@ -6,6 +6,8 @@ func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
 	add_autoload_singleton("VTGlobal", "res://addons/vterrariumframework/src/globals/vtglobal.gd")
 	add_autoload_singleton("VTConfig", "res://addons/vterrariumframework/src/globals/vtconfig.gd")
+	add_autoload_singleton("VTUserConfig", "res://addons/vterrariumframework/src/globals/vt_user_config.gd")
+	add_autoload_singleton("VTHardware", "res://addons/vterrariumframework/src/globals/vt_hardware.gd")
 
 
 func _exit_tree() -> void:
