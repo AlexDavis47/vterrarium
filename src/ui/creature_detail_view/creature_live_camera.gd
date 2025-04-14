3e602d225daf85ee7683f5dbccff16e8c3bb97f1
Alex Davis <a_davis47@u.pacific.edu>
2025-04-14 13:54:25 -0700

    some fixes to our weird faces

--- src/ui/creature_detail_view/creature_live_camera.gd
+++ src/ui/creature_detail_view/creature_live_camera.gd
@@ -13,9 +13,9 @@ func _process(delta: float) -> void:
 	if creature_data and creature_data.creature_is_in_tank:
 		if creature_instance:
 			# Calculate camera position based on creature's rotation
-			var offset = Vector3(0, 0.5, 0.5).rotated(Vector3.UP, creature_instance.rotation.y)
+			var offset = Vector3(0, 0.1, 0.5).rotated(Vector3.UP, creature_instance.rotation.y)
 			offset = offset.rotated(Vector3.UP, 60)
 			look_at_from_position(creature_instance.global_position + offset, creature_instance.global_position, Vector3.UP)
-			global_position += Vector3(0, 0.3, 0)
+			global_position += Vector3(0, 0.1, 0)
 		else:
 			creature_instance = creature_data.creature_instance
